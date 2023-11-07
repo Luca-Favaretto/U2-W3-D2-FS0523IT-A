c9af550f6a41714a8e65301ba0df375087d33fc3
Luca-Favaretto <145663602+Luca-Favaretto@users.noreply.github.com>
2023-11-07 15:37:14 +0100

    Finish exercise

---
+++ js/script.js
@@ -0,0 +1,52 @@
+window.addEventListener("DOMContentLoaded", () => {
+  let name = localStorage.getItem("newName");
+  if (!name) {
+    name = "Nessun valore attuale";
+  }
+  console.log(name);
+  let valueToModifi = document.getElementById("current-value");
+  valueToModifi.innerText = name;
+  const GREEN_BTN = document.getElementById("save");
+  const RED_BTN = document.getElementById("remove");
+  GREEN_BTN.onclick = addLocalName;
+  RED_BTN.onclick = removeLocalName;
+
+  //////////////////////////////////////////
+  newItemDate();
+  const TIME_BTN = document.querySelector('button[type="button"] ');
+  TIME_BTN.onclick = newItemDate;
+  setInterval(timePassed, 200);
+});
+
+const addLocalName = function (e) {
+  e.preventDefault();
+  let valueToTake = document.getElementById("nome").value;
+  localStorage.setItem("newName", valueToTake);
+  let valueToModifi = document.getElementById("current-value");
+  valueToModifi.innerText = valueToTake;
+  console.log(valueToTake);
+};
+
+const removeLocalName = function (e) {
+  e.preventDefault();
+  let valueToModifi = document.getElementById("current-value");
+  let valueToTake = document.getElementById("nome");
+  localStorage.removeItem("newName");
+  valueToModifi.innerText = "";
+  valueToTake.value = "";
+};
+const newItemDate = function () {
+  localStorage.setItem("date-item", new Date());
+};
+const timePassed = function () {
+  const firstDate = new Date(localStorage.getItem("date-item"));
+  const diffTime = Math.floor((new Date() - firstDate) / 1000);
+  console.log(diffTime);
+  const divTime = document.getElementById("timer");
+  while (divTime.firstChild) {
+    divTime.firstChild.remove();
+  }
+  let p = document.createElement("p");
+  p.innerHTML = diffTime;
+  divTime.appendChild(p);
+};
